ae6b476973fe2d2170b8379cc8dedc388b123b2b
itjake <tokonov.tagai@gmail.com>
2022-07-27 19:21:31 +0600

    feat: added CSS for thirdPage in MainPage

--- src/pages/MainPage/MainThirdPage/MainThirdPage.js
+++ src/pages/MainPage/MainThirdPage/MainThirdPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styles from "./MainThirdPage.module.css";
 import car from "../../../Assets/img/car.png";
+import plant from "../../../Assets/img/plant.png";
+import ring from "../../../Assets/img/ring.png";
+import location from "../../../Assets/img/location.png";
 
 const MainThirdPage = () => {
   return (
@@ -21,6 +24,40 @@ const MainThirdPage = () => {
               <div className={styles.thirdPageDescription}>
                 Доставим заказанную вами
                 еду за 30 минут + напиток в подарок
+              </div>
+            </div>
+            <div className={styles.thirdPageProduct}>
+              <div className={styles.thirdPageImg}>
+                <img src={plant} alt="plant"/>
+              </div>
+              <div className={styles.thirdPageName}>
+                Свежие продукты
+              </div>
+              <div className={styles.thirdPageDescription}>
+                Вся продукция хранится в хороших условиях тем самым продливая срок хранения
+              </div>
+            </div>
+            <div className={styles.thirdPageProduct}>
+              <div className={styles.thirdPageImg}>
+                <img src={ring} alt="ring"/>
+              </div>
+              <div className={styles.thirdPageName}>
+                Уникальное меню
+              </div>
+              <div className={styles.thirdPageDescription}>
+                Ежедневно мы обновляем наше
+                меню и том числе коктейльное
+              </div>
+            </div>
+            <div className={styles.thirdPageProduct}>
+              <div className={styles.thirdPageImg}>
+                <img src={location} alt="location"/>
+              </div>
+              <div className={styles.thirdPageName}>
+                Доставка
+              </div>
+              <div className={styles.thirdPageDescription}>
+                Мы быстро доставляем вашу еду по указанному адресу
               </div>
             </div>
           </div>
