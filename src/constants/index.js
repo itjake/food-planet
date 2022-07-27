8bf175c4242438e98ab93ca250c0eb040e5301c1
itjake <tokonov.tagai@gmail.com>
2022-07-27 17:59:58 +0600

    feat: created constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,36 @@
+import burgerFirst from "../Assets/img/burger_first.png";
+import burgerSecond from "../Assets/img/burger_second.png";
+import burgerThird from "../Assets/img/burger_third.png";
+import burgerFourth from "../Assets/img/burger_fourth.png";
+
+
 export const burger = [
   {
     id: Math.random().toString(),
-    img: "",
-    description: "",
-    price: "",
+    img: burgerFirst,
+    name: "Чизбургер",
+    description: "Булка, котлета,сыр,соленый огурец,лук,помидор,салат айсберг,соус чесночный,соус гриль,кетчуп,майонез",
+    price: "200  сом",
+  },
+  {
+    id: Math.random().toString(),
+    img: burgerSecond,
+    name: "Чизбургер",
+    description: "Булка, котлета,сыр,соленый огурец,лук,помидор,салат айсберг,соус чесночный,соус гриль,кетчуп,майонез",
+    price: "200  сом",
+  },
+  {
+    id: Math.random().toString(),
+    img: burgerThird,
+    name: "Чизбургер",
+    description: "Булка, котлета,сыр,соленый огурец,лук,помидор,салат айсберг,соус чесночный,соус гриль,кетчуп,майонез",
+    price: "200  сом",
+  },
+  {
+    id: Math.random().toString(),
+    img: burgerFourth,
+    name: "Чизбургер",
+    description: "Булка, котлета,сыр,соленый огурец,лук,помидор,салат айсберг,соус чесночный,соус гриль,кетчуп,майонез",
+    price: "200  сом",
   }
 ]
