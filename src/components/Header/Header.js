747a9102ecd805f048584809b5907320280f69a2
Tokonov Tagai <tokonov.tagai@gmail.com>
2022-08-10 15:50:46 +0600

    refactor: made header as a main component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,45 +1,55 @@
 import React, {useState} from 'react';
-import {NavLink} from "react-router-dom";
+import {NavLink, Outlet} from "react-router-dom";
 import styles from "./Header.module.css";
 import headerLogo from "../../Assets/img/headerLogo.png"
 import bundle from "../../Assets/icons/bundle.svg"
 import tel from "../../Assets/icons/tel.svg"
+import Footer from "../Footer/Footer";
 
 const Header = () => {
-  const [isActive, setIsActive] = useState(false);
+    const [isActive, setIsActive] = useState(false);
 
-  window.onscroll = () => {
-    if (window.pageYOffset > 50) {
-      setIsActive(true);
-    } else {
-      setIsActive(false);
+    window.onscroll = () => {
+        if (window.pageYOffset > 50) {
+            setIsActive(true);
+        } else {
+            setIsActive(false);
+        }
     }
-  }
-  console.log(isActive);
 
-  return (
-    <div className={isActive ? styles.headerActive : ""} id="headerContent">
-      <div className={styles.headerWrapper}>
-        <div className={styles.headerLogo} id="headerLogo">
-          <img src={headerLogo} alt="headerLogo"/>
-        </div>
-        <ul className={styles.headerMenu}>
-          <li><NavLink to="/" className={styles.headerItem}>Главная</NavLink></li>
-          <li><NavLink to="/menu" className={styles.headerItem}>Меню</NavLink></li>
-          <li><NavLink to="/order" className={styles.headerItem}>Доставка</NavLink></li>
-          <li><NavLink to="/contacts" className={styles.headerItem}>Контакты</NavLink></li>
-          <li className={styles.headerItemTel}>
-            <img src={tel} alt="" className={styles.headerTel}/>
-            +996500405988
-          </li>
-          <li className={styles.headerItemBundle}>
-            <img src={bundle} alt="bundle" className={styles.bundleOne}/>
-            1
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+    return (<>
+            <section className="container">
+                <div className="content">
+                    <div className={isActive ? styles.headerActive : ""} id="headerContent">
+
+                        <div className={styles.headerWrapper}>
+                            <div className={styles.headerLogo} id="headerLogo">
+                                <img src={headerLogo} alt="headerLogo"/>
+                            </div>
+                            <ul className={styles.headerMenu}>
+                                <li><NavLink to="/" className={styles.headerItem}>Главная</NavLink></li>
+                                <li><NavLink to="/menu" className={styles.headerItem}>Меню</NavLink></li>
+                                <li><NavLink to="/order" className={styles.headerItem}>Доставка</NavLink></li>
+                                <li><NavLink to="/contacts" className={styles.headerItem}>Контакты</NavLink></li>
+                                <li className={styles.headerItemTel}>
+                                    <img src={tel} alt="" className={styles.headerTel}/>
+                                    +996500405988
+                                </li>
+                                <li>
+                                    <NavLink to="/cart" className={styles.headerItem}>
+                                        <img src={bundle} alt="bundle" className={styles.bundleOne}/>
+                                        1
+                                    </NavLink>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <Outlet/>
+                </div>
+            </section>
+            <Footer/>
+        </>
+    );
 };
 
 export default Header;
