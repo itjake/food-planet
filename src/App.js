ce05e2482eae403cb0b8bdd9f7e254e95aab7a63
Tokonov Tagai <tokonov.tagai@gmail.com>
2022-08-10 15:52:10 +0600

    refactor: changed route position

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {Toaster} from "react-hot-toast";
 import './App.css';
 import Header from "./components/Header/Header";
 import Main from "./pages/MainPage/Main";
@@ -7,25 +8,28 @@ import Footer from "./components/Footer/Footer";
 import MainMenu from "./pages/MainPage/components/MainMenu/MainMenu";
 import MainOrder from "./pages/MainPage/components/MainOrder/MainOrder";
 import NotFound from "./pages/NotFound/NotFound";
+import Products from "./pages/Admin/Products/Products";
+import Cart from "./pages/Cart/Cart";
 
 function App() {
     return (
         <>
-            <section className="container">
-                <div className="content">
-                    <BrowserRouter>
-                        <Header/>
-                        <Routes>
-                            <Route path="/" element={<Main/>}/>
-                            <Route path="/menu" element={<MainMenu/>}/>
-                            <Route path="/order" element={<MainOrder/>}/>
-                            <Route path="/contacts" element={<Main/>}/>
-                            <Route path="*" element={<NotFound/>}/>
-                        </Routes>
-                    </BrowserRouter>
-                </div>
-            </section>
-            <Footer/>
+
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Header/>}>
+                        <Route index element element={<Main/>}/>
+                        <Route path="/menu" element={<MainMenu/>}/>
+                        <Route path="/order" element={<MainOrder/>}/>
+                        <Route path="/contacts" element={<Main/>}/>
+                        <Route path="/cart" element={<Cart/>}/>
+                    </Route>
+
+                    <Route path="/admin/check-products" element={<Products/>}/>
+                    <Route path="*" element={<NotFound/>}/>
+                </Routes>
+                {Toaster}
+            </BrowserRouter>
         </>
     );
 }
