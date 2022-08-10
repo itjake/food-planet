1d3dc05ed7f1c46b4e707b152d199e2112cdfe7b
Tokonov Tagai <tokonov.tagai@gmail.com>
2022-08-10 15:49:28 +0600

    refactor: deleted ReactDom portal

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,56 +1,28 @@
 import React from 'react';
 import styles from "./NotFound.module.css";
-import ReactDom from "react-dom";
+import {Link} from "react-router-dom";
 
 const NotFound = () => {
 
-    const returnToMainPage = () => {
-        window.location.assign('http://localhost:3000/');
-    }
-
-    document.getElementById('root').className = styles.root;
-
-    const A = () => {
-        return (
-            <section className={styles.errorPage}>
-                <div className={styles.errorContent}>
-                    <div className={styles.errorTitle}>
-                        404
-                    </div>
-                    <div className={styles.errorSubTitle}>
-                        Страница не найдена
-                    </div>
-                    <div className={styles.errorDescription}>
-                        Ой! Страница, которую вы ищете, не существует. Возможно, он был перемещен или удален.
-                    </div>
-                    <div className={styles.errorButton}>
-                        <button onClick={returnToMainPage} className={styles.errorBtn}>Вернуться на главную</button>
-                    </div>
+    return (
+        <section className={styles.errorPage}>
+            <div className={styles.errorContent}>
+                <div className={styles.errorTitle}>
+                    404
                 </div>
-            </section>
-        );
-    }
-
-
-    return (<>
-            {ReactDom.createPortal(<A/>, document.getElementById("ola"))}
-            {/*<section className={styles.errorPage}>*/}
-            {/*    <div className={styles.errorContent}>*/}
-            {/*        <div className={styles.errorTitle}>*/}
-            {/*            404*/}
-            {/*        </div>*/}
-            {/*        <div className={styles.errorSubTitle}>*/}
-            {/*            Страница не найдена*/}
-            {/*        </div>*/}
-            {/*        <div className={styles.errorDescription}>*/}
-            {/*            Ой! Страница, которую вы ищете, не существует. Возможно, он был перемещен или удален.*/}
-            {/*        </div>*/}
-            {/*        <div className={styles.errorButton}>*/}
-            {/*            <Link to="*" className={styles.errorBtn} onClick={returnToMainPage}>Вернуться на главную</Link>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</section>*/}
-        </>
+                <div className={styles.errorSubTitle}>
+                    Страница не найдена
+                </div>
+                <div className={styles.errorDescription}>
+                    Ой! Страница, которую вы ищете, не существует. Возможно, он был перемещен или удален.
+                </div>
+                <div className={styles.errorButton}>
+                    <Link to="/">
+                        <button className={styles.errorBtn}>Вернуться на главную</button>
+                    </Link>
+                </div>
+            </div>
+        </section>
     );
 };
 
